104f3c84d5bd6b6a79a0bd7c6d16850cd1dbb059
Abrosss <nataroro5@gmail.com>
2022-12-14 21:42:58 +0600

    changed

--- src/components/GoogleAuthButton.js
+++ src/components/GoogleAuthButton.js
@@ -4,7 +4,7 @@ import { GoogleLogin } from "@react-oauth/google";
 const GoogleAuthButton = ({ onSuccess, onFailure }) => (
   <GoogleLogin
   clientId="730246444349-9hq6kctetsldg9sd6ssiiot1d476oje8.apps.googleusercontent.com"
-  redirectUri="http://localhost:5000/auth/google/callback"
+  redirectUri="https://galleria-one.vercel.app/auth/google/callback"
   onSuccess={onSuccess}
   onFailure={onFailure}
   className="google-auth-button"
