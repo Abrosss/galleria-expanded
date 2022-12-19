355493c7c474b13a0ce456d7a32008acd96edefe
Abrosss <nataroro5@gmail.com>
2022-12-19 09:25:01 +0600

    update

--- src/components/ImageCardArt.js
+++ src/components/ImageCardArt.js
@@ -2,50 +2,60 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { useContext } from 'react';
-import axios from '../api/axios';
 import ArtToBeEditedContext from '../context/EditedArt';
 import dots from '../shared/dots.svg'
-function ImageCardArt({img, index, deletePicture}) {
+function ImageCardArt({ img, index, deletePicture }) {
 
-    const navigate = useNavigate();
-    const [hovered, setHovered] = useState(null)
-    const [settingsPopup, setSettingsPopup] = useState(null)
-    const {editPopup, setEditPopup } = useContext(ArtToBeEditedContext);
-    console.log(editPopup)
-      const viewImage = (img, i, e) => {
-        console.log(e.target.tagName)
-        if(!e.target.dataset.id && e.target.tagName !== 'LI') {
-          navigate('/slideshow',{state: {
-            art:{img, i},
-            slide:false
-          } });
+  const navigate = useNavigate();
+  const [hovered, setHovered] = useState(null)
+  const [settingsPopup, setSettingsPopup] = useState(null)
+  const { editPopup, setEditPopup } = useContext(ArtToBeEditedContext);
+
+  const viewImage = (img, i, e) => {
+    console.log(e.target.tagName)
+    if (!e.target.dataset.id && e.target.tagName !== 'LI') {
+      navigate('/slideshow', {
+        state: {
+          art: { img, i },
+          slide: false
         }
-        
-    
-      }
-      
- 
-    return (
-        <div  onClick={(e) => viewImage(img, index, e)} key={index} className="image">
-         
-        <div onMouseEnter={() => setHovered(index)} onMouseLeave={() => setHovered(null)} className='image-container'>
-       
-        <div data-id={img._id} onClick={() => setSettingsPopup(prev => prev === null ? index : null
-              )} className={hovered === index ? 'settings show' : 'settings'}><img data-id={img._id} src={dots}></img>
-                <ul className={settingsPopup === index ? ' settingsPopup show' : 'settingsPopup'}>
-                  <li onClick={() => setEditPopup(img)}>Edit</li>
-                  <li onClick={() => deletePicture(img._id)}>Delete</li>
-                </ul>
+      });
+    }
 
-              </div>
-      <img className='img dimmed' src={img.image} alt="thumbnail"   ></img>
-      <div className='caption'>
-        <h2>{img.title}</h2>
-        <p>{img.artist && img.artist.name}</p>
-      </div>
+
+  }
+  function onMouseEnter(index) {
+    setHovered(index)
+  }
+
+  function onMouseLeave() {
+    setHovered(null)
+  }
+
+  return (
+    <div onClick={(e) => viewImage(img, index, e)} key={index} className="image">
+
+      <div 
+      onMouseEnter={() => onMouseEnter(index)} 
+      onMouseLeave={onMouseLeave} 
+      className='image-container'>
+
+        <div data-id={img._id} onClick={() => setSettingsPopup(prev => prev === null ? index : null
+        )} className={hovered === index ? 'settings show' : 'settings'}><img data-id={img._id} src={dots} alt='settings icon'></img>
+          <ul className={settingsPopup === index ? ' settingsPopup show' : 'settingsPopup'}>
+            <li onClick={() => setEditPopup(img)}>Edit</li>
+            <li onClick={() => deletePicture(img._id)}>Delete</li>
+          </ul>
+
+        </div>
+        <img className='img dimmed' src={img.image} alt="thumbnail"   ></img>
+        <div className='caption'>
+          <h2>{img.title}</h2>
+          <p>{img.artist && img.artist.name}</p>
+        </div>
       </div>
     </div>
-    )
+  )
 }
 
 export default ImageCardArt
